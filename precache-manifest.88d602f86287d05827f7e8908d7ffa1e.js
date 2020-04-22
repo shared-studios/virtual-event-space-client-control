2d38cbb96410322cceaacda008d02fd7adc11502
Siam Mridha <siam@sharedstudios.com>
2020-04-22 19:09:44 -0400

    Updates

---
+++ precache-manifest.88d602f86287d05827f7e8908d7ffa1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb4560d3ed983b229a4c6d0d854fe2a",
+    "url": "/virtual-event-space-client-control/index.html"
+  },
+  {
+    "revision": "a087c427dae1b3b09734",
+    "url": "/virtual-event-space-client-control/static/css/main.f612f612.chunk.css"
+  },
+  {
+    "revision": "cd23404751df8565f695",
+    "url": "/virtual-event-space-client-control/static/js/2.63e9fbb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/virtual-event-space-client-control/static/js/2.63e9fbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a087c427dae1b3b09734",
+    "url": "/virtual-event-space-client-control/static/js/main.eb2fc36b.chunk.js"
+  },
+  {
+    "revision": "557fc7fa3cc567df523d",
+    "url": "/virtual-event-space-client-control/static/js/runtime-main.e7289ecf.js"
+  }
+]);
