469d7abf6f43692ce2ed549de2d243d8e7e7c015
Siam Mridha <siam@sharedstudios.com>
2020-04-22 20:43:08 -0400

    no message

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './styles.module.css'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const Error = () => {
     const error = useSelector(state => state.error)
-    const dispatch = useDispatch()
-
     return (
         <div className={styles.error}>{error}</div>
     )
