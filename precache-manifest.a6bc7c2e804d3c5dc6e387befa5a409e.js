0ef4ecf9f6f6869abb82b22ab2f8b9cb60bd867f
Siam Mridha <siam@sharedstudios.com>
2020-05-06 05:52:25 -0400

    Updates

---
+++ precache-manifest.a6bc7c2e804d3c5dc6e387befa5a409e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cdc0898f30a82bcffa37d6e83d685b",
+    "url": "/virtual-event-space-client-control/index.html"
+  },
+  {
+    "revision": "27438574ba254606f76d",
+    "url": "/virtual-event-space-client-control/static/css/main.32ec79f7.chunk.css"
+  },
+  {
+    "revision": "49e4ac0a8649d7273413",
+    "url": "/virtual-event-space-client-control/static/js/2.1a1ef559.chunk.js"
+  },
+  {
+    "revision": "ca1bd915694df59b65d9523ab71b2a32",
+    "url": "/virtual-event-space-client-control/static/js/2.1a1ef559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27438574ba254606f76d",
+    "url": "/virtual-event-space-client-control/static/js/main.e1f58089.chunk.js"
+  },
+  {
+    "revision": "8b6c65f6eebd796b2c76",
+    "url": "/virtual-event-space-client-control/static/js/runtime-main.3df4e139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/virtual-event-space-client-control/precache-manifest.8d04148f14f997676671e4ab15e1d905.js"
+  "/virtual-event-space-client-control/precache-manifest.a6bc7c2e804d3c5dc6e387befa5a409e.js"
 );
 
 self.addEventListener('message', (event) => {
