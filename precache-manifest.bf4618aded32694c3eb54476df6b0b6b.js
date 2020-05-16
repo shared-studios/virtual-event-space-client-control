0f1e6c0d8ae4e724ac29c6291704aa6e2d4e0820
Siam Mridha <siam@sharedstudios.com>
2020-05-16 07:59:29 -0400

    Updates

--- precache-manifest.bf4618aded32694c3eb54476df6b0b6b.js
+++ precache-manifest.bf4618aded32694c3eb54476df6b0b6b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9fb3ea0abb2152a2c30e1215183b0a57",
+    "revision": "60d65b0edbe4205015795efdc611af79",
     "url": "/virtual-event-space-client-control/index.html"
   },
   {
-    "revision": "ae92993b41d8112fa1ec",
+    "revision": "0c73d18bd34bc7b8e682",
     "url": "/virtual-event-space-client-control/static/css/main.d0e51f16.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/virtual-event-space-client-control/static/js/2.ee02130d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ae92993b41d8112fa1ec",
-    "url": "/virtual-event-space-client-control/static/js/main.68d275bd.chunk.js"
+    "revision": "0c73d18bd34bc7b8e682",
+    "url": "/virtual-event-space-client-control/static/js/main.ef5588c1.chunk.js"
   },
   {
     "revision": "8b6c65f6eebd796b2c76",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/virtual-event-space-client-control/precache-manifest.66ec8cea9fd510a598450a13caa84525.js"
+  "/virtual-event-space-client-control/precache-manifest.bf4618aded32694c3eb54476df6b0b6b.js"
 );
 
 self.addEventListener('message', (event) => {
